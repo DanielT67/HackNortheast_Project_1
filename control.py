a1b31504ed81ad99410de9705238a668fc03a140
Daniel Tkachov <Daniel_Tkachov@student.uml.edu>
2021-01-17 10:43:17 -0500

    Live is true, past is false now

--- control.py
+++ control.py
@@ -4,12 +4,12 @@ from getPastTweets import get_past_tweets
 if __name__ == '__main__':
     control_ticker = input("Enter Ticker Symbol for Company: ")
     selection = input("Type \'Live\' to see Live Tweets or type \'Past\' to see past tweets:")
-    while selection == 'Live' or selection == 'Past':
-        if selection == 'Live':
+    while selection == True or selection == False:
+        if selection == True:
             print("Now viewing live tweets:")
             get_live_tweets(control_ticker)
             break
-        if selection == 'Past':
+        if selection == False:
             tweets_to_open = int(input("How many tweets would you like to retrieve? "))
             days_past = int(input("What is the age limit of the tweets in days? "))
             get_past_tweets(control_ticker, tweets_to_open, days_past)
