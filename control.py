3b4f78c7981971eca1f965b683b3e0c5ca8b479d
Daniel Tkachov <Daniel_Tkachov@student.uml.edu>
2021-01-17 10:53:22 -0500

    Changed control.py to work with javascript

--- control.py
+++ control.py
@@ -2,16 +2,14 @@ from getLiveTweet import get_live_tweets
 from getPastTweets import get_past_tweets
 
 if __name__ == '__main__':
-    control_ticker = input("Enter Ticker Symbol for Company: ")
-    selection = input("Type \'Live\' to see Live Tweets or type \'Past\' to see past tweets:")
-    while selection == True or selection == False:
-        if selection == True:
-            print("Now viewing live tweets:")
-            get_live_tweets(control_ticker)
-            break
-        if selection == False:
-            tweets_to_open = int(input("How many tweets would you like to retrieve? "))
-            days_past = int(input("What is the age limit of the tweets in days? "))
-            get_past_tweets(control_ticker, tweets_to_open, days_past)
-            print("\nAll tweets are stored in a json for easy access.")
-            break
+    stockName = request.form['text']
+    isLive = request.form['isLive']
+    if isLive == True:
+        print("Now viewing live tweets:")
+        get_live_tweets(stockName)
+    else:
+        tweets_to_open = request.form['tweets_to_open']
+        days_past = request.form['days_past']
+        get_past_tweets(stockName, int(tweets_to_open), int(days_past))
+        print("\nAll tweets are stored in a json for easy access.")
+
